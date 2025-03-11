09e3c8621e369a1f45d194656cd9d8469c767ce4
wayyzt <1079828406@qq.com>
2025-03-11 12:27:21 +0800

    modify(.sh): remove machine dir in .sh

--- run-predict.sh
+++ run-predict.sh
@@ -2,7 +2,7 @@
 python predict.py \
     --input_data_dir data/split-target-inter-pure6/test.csv \
     --output_data_dir predict-split-inter.csv \
-    --dir_save_model /datapool/data2/home/majianzhu/xinheng/xiangzhen/DTI2/DTI-tfcpi2/run/20250303-1045_split-target-inter-pure6/best_acc.pth \
+    --dir_save_model run/20250303-1045_split-target-inter-pure6/best_acc.pth \
     --fetch_pretrained_target \
     --hid_dim 128 \
     --dropout 0 \
@@ -13,7 +13,7 @@ python predict.py \
 python predict.py \
     --input_data_dir data/split-random/test.csv \
     --output_data_dir predict-split-random.csv \
-    --dir_save_model /datapool/data2/home/majianzhu/xinheng/xiangzhen/DTI2/DTI-tfcpi2/run/20250221-1942_split-random/best_acc.pth \
+    --dir_save_model run/20250221-1942_split-random/best_acc.pth \
     --fetch_pretrained_target \
     --batchsize 32 \
     --cuda_use cuda:7 \
@@ -22,7 +22,7 @@ python predict.py \
 python predict.py \
     --input_data_dir data/split-target-intra/test.csv \
     --output_data_dir predict-split-intra.csv \
-    --dir_save_model /datapool/data2/home/majianzhu/xinheng/xiangzhen/DTI2/DTI-tfcpi2/run/20250222-1318_split-target-intra/best_acc.pth \
+    --dir_save_model run/20250222-1318_split-target-intra/best_acc.pth \
     --fetch_pretrained_target \
     --batchsize 32 \
     --cuda_use cuda:7 \
